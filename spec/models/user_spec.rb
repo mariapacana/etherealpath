0d2a42b6062f1c617fb9a0e75743a3ad46effcf2
Maria Pacana <maria.pacana@gmail.com>
2014-12-28 18:05:18 -0800

    Deleted test/ folder because app now uses rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,6 @@ require "rails_helper"
 
 RSpec.describe User, :type => :model do
   describe "#initialize" do
-    it { should have_many(:phone_numbers)}
-    it { should belong_to(:mission)}
     it { should validate_presence_of(:first_name)}
     it { should validate_presence_of(:last_name)}
     it { should validate_presence_of(:email)}
