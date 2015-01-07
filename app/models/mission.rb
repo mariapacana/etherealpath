b54df106808c4f4b07264df7fc53addc74171168
Maria Pacana <maria.pacana@gmail.com>
2015-01-07 10:26:14 -0800

    When a new mission is created, all participants are set to the first challenge.

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,4 +3,10 @@ class Mission < ActiveRecord::Base
   accepts_nested_attributes_for :challenges, :reject_if => :all_blank, :allow_destroy => true
 
   validates :title, :description, presence: true
+  after_create :assign_participants_to_first_challenge
+
+  private
+    def assign_participants_to_first_challenge
+      Participant.all.each {|p| p.update_attribute(:challenge_id, self.challenges.first.id)}
+    end
 end
