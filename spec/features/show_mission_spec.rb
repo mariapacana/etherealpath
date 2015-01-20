93a2e8b1b3af6fc19df21471d501b14e813907c4
Maria Pacana <maria.pacana@gmail.com>
2015-01-19 16:23:22 -0800

    Updated Mission show spec and view

---
+++ spec/features/show_mission_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+feature "creating new missions" do
+  background do
+    @user = User.create({first_name: "Bob",
+                        last_name: "Hob",
+                        email: "bob@hob.com",
+                        password: "bobbob",
+                        password_confirmation: "bobbob"})
+    @mission = Mission.create({title: "Awesome Mission",
+                               description: "Weird Time",
+                               intro: "You have a choice",
+                               warning: "Be very afraid",
+                               decline_confirmation: "OK fine",
+                               finish_confirmation: "Congrats",
+                               location_invite: "rooted, SF, or Ebay?",
+                               completed_challenges_required: 3,
+                               start_time: DateTime.new(2020,1,1)})
+    @challenge1 = @mission.challenges.create({location: "SF",
+                                            question: "blah",
+                                            response_success: "blah",
+                                            response_failure: "blah",
+                                            any_answer_acceptable: false})
+    @answer1 = @challenge1.answers.create({text: "OK"})
+  end
+
+  scenario "logged-in user can create a new mission" do
+    page.set_rack_session(:user_id => @user.id)
+    page.visit '/missions/1'
+    expect(page).to have_selector("h1", text: @mission.title)
+    expect(page).to have_selector("li p", text: @mission.start_time)
+    expect(page).to have_selector("li p", text: @mission.description)
+    expect(page).to have_selector("li p", text: @mission.intro)
+    expect(page).to have_selector("li p", text: @mission.warning)
+    expect(page).to have_selector("li p", text: @mission.decline_confirmation)
+    expect(page).to have_selector("li p", text: @mission.finish_confirmation)
+    expect(page).to have_selector("li p", text: @mission.location_invite)
+    expect(page).to have_selector("li p", text: @mission.completed_challenges_required)
+    expect(page).to have_selector("h3", text: "Challenges")
+    expect(page).to have_selector("li p", text: @challenge1.question)
+    expect(page).to have_selector("li p", text: @challenge1.response_success)
+    expect(page).to have_selector("li p", text: @challenge1.response_failure)
+    expect(page).to have_selector("li p", text: @challenge1.any_answer_acceptable)
+    expect(page).to have_selector("li b", text: "Answers")
+    expect(page).to have_selector("li ul li p", text: @answer1.text)
+  end
+
+end
